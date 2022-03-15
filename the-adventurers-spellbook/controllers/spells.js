c3b660d545661cd150720936d0e62e315b0822bc
patrickga <patrick.g.alvarez@gmail.com>
2022-03-15 08:31:40 -0500

    Adds a post route to create a new spell

--- the-adventurers-spellbook/controllers/spells.js
+++ the-adventurers-spellbook/controllers/spells.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const fetch = require('node-fetch')
 const Character = require('../models/character')
+const Spell = require('../models/spell')
 
 // Create router
 const router = express.Router()
@@ -22,10 +23,36 @@ router.use((req, res, next) => {
 
 //Routes
 
-//new route to create a new spell
+//new route to the create new spell form
 router.get('/new', (req, res)=>{
     res.render('spells/new')
 })
+
+//route to create a new spell
+router.post('/', (req, res)=>{
+	req.body.concentration = req.body.concentration === "on" ? true : false 
+	req.body.owner = req.session.userId
+	Spell.create(req.body)
+		.then(data =>{
+			console.log('this is the new spell that was created', data)
+			res.redirect('spells/new')
+		})
+})
+
+// index that shows only spells the user created
+router.get('/mine', (req, res) => {
+    // destructure user info from req.session
+    const { username, userId, loggedIn } = req.session
+	Spell.find({ owner: userId })
+		.then(spells => {
+			res.render('spells/index', { spells, username, loggedIn })
+		})
+		.catch(error => {
+			res.redirect(`/error?error=${error}`)
+		})
+})
+
+
 
 //index that shows all spells of the character's class
 router.get('/:id', (req, res)=>{
